0309e93dd97760dd3cc7183ffd0abec6109d0ca5
Jaremy Creechley <creechley@gmail.com>
2020-05-15 02:19:11 -0600

    native updates

---
+++ nim/extras.h
@@ -0,0 +1,6 @@
+
+#define IN6_IS_ADDR_V4MAPPED(a) \
+        ((((__const uint32_t *) (a))[0] == 0)                                 \
+         && (((__const uint32_t *) (a))[1] == 0)                              \
+         && (((__const uint32_t *) (a))[2] == htonl (0xffff)))
+
