67d5c77675f79ab7c445c03aeaca3e65a3f25f35
Jaremy Creechley <creechley@gmail.com>
2020-05-15 02:17:07 -0600

    native updates

--- nim/_nimcache/stdlib_nativesockets.nim.c
+++ nim/_nimcache/stdlib_nativesockets.nim.c
@@ -15,6 +15,12 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/un.h>
+
+#define IN6_IS_ADDR_V4MAPPED(a) \
+        ((((__const uint32_t *) (a))[0] == 0)                                 \
+         && (((__const uint32_t *) (a))[1] == 0)                              \
+         && (((__const uint32_t *) (a))[2] == htonl (0xffff)))
+
 #undef LANGUAGE_C
 #undef MIPSEB
 #undef MIPSEL
